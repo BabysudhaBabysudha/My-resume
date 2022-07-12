9c8fe0cd76b5ef6b2ae32ab4feeac6a26b2cb492
BabysudhaBabysudha <p.sudha0100@gmail.com>
2022-07-12 17:17:25 +0530

    initial commit

---
+++ src/Resume/Session.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import Contact from './Contact';
+
+function Session() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default Session;
---
+++ src/Resume/image.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Myphoto from '../Resume/Babysudha.jpg';
+ import './index.css'
+
+export default function Image() {
+  return (
+    <div className='img-wrapper'>
+        <img src={Myphoto}
+            alt='myphoto'
+            width='300px'
+            height='300px' />
+    </div>
+  )
+}
